f902044fc63787afe0adf3f4a1dff317f4342f5f
hvr.blackswan <hvr.blackswan@gmail.com>
2022-11-18 04:26:11 +0700

    added storybook

---
+++ src/widgets/NavBar/UI/NavBar.stories.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from 'app/providers/ThemeProvider';
+import { NavBar } from './NavBar';
+
+export default {
+  title: 'widget/NavBar',
+  component: NavBar,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as ComponentMeta<typeof NavBar>;
+
+const Template: ComponentStory<typeof NavBar> = (args) => <NavBar {...args} />;
+
+export const Light = Template.bind({});
+Light.args = {
+};
+
+export const Dark = Template.bind({});
+Dark.args = {
+};
+Dark.decorators = [ThemeDecorator(Theme.DARK)];
